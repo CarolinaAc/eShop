48b65cfeb60ed73d946d4c24d0081950805f0106
Carolina Acosta <carolinaacosta.nuez@gmail.com>
2020-05-01 11:44:01 +0100

    made navbar prototype

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
+//import Header from './components/Header'
 import Books from './components/Books'
+import Header from './components/Header'
 
 function App() {
   return (
